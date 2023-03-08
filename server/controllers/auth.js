3cac1bb8d2856ab8f53e4994a7ca754b0a148835
Holden Matthews <holdenmatthews1@gmail.com>
2023-03-08 09:08:45 -0700

    part 4 step 2 complete

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -42,7 +42,30 @@ module.exports = {
         }
     },
 
-    login: (req, res) => {
-        console.log('login')
+    login: async (req, res) => {
+        try {
+            const { username, password } = req.body
+            const foundUser = await User.findOne({where: {username: username}})
+            if (foundUser) {
+                const isAuthenticated = bcrypt.compareSync(password, foundUser.hashedPass)
+                if (isAuthenticated) {
+                    const token = createToken(newUser.dataValues.username, newUser.dataValues.id)
+                    const exp = Date.now() + 1000 * 60 * 60 * 48
+                    res.status(200).send({
+                        username: newUser.dataValues.username,
+                        userId: newUser.dataValues.id,
+                        token,
+                        exp
+                    })
+                } else {
+                    res.status(400).send('cannot log in')
+                }
+            } else {
+                res.status(400).send('cannot log in')
+            }
+        } catch (err) {
+            console.log(err)
+            res.status(400).send(err)
+        }
     }
 }
