1349e23e061d8b5b3faf158d3dcf2803db2789c4
Holden Matthews <holdenmatthews1@gmail.com>
2023-03-08 09:00:43 -0700

    part 4 step 1 complete

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,45 @@
+require('dotenv').config()
+const { SECRET } = process.env
+const { User } = require('../models/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const createToken = (username, id) => {
+    jwt.sign(
+        {
+            username, 
+            id
+        },
+        SECRET,
+        {
+            expiresIn: '2 Days'
+        })
+}
+
 module.exports = {
-    register: (req, res) => {
-        console.log('register')
+    register: async (req, res) => {
+        try {
+            const { username, password } = req.body
+            const foundUser = await User.findOne({where: {username: username}})
+            if (foundUser) {
+                res.status(400).send('cannot create user - that username is already taken')
+            } else {
+                const salt = bcrypt.genSaltSync(10)
+                const hash = bcrypt.hashSync(password, salt)
+                const newUser = await User.create({username: username, hashedPass: hash})
+                const token = createToken(newUser.dataValues.username, newUser.dataValues.id)
+                const exp = Date.now() + 1000 * 60 * 60 * 48
+                res.status(200).send({
+                    username: newUser.dataValues.username,
+                    userId: newUser.dataValues.id,
+                    token,
+                    exp
+                })
+            }
+        } catch (err) {
+            console.log(err)
+            res.status(400).send(err)
+        }
     },
 
     login: (req, res) => {
